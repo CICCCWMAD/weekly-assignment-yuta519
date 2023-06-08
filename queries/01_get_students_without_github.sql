d1d3bece0e654fbe40d8f25e8aa15e9065992fc8
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-08 16:35:21 +0900

    FIX🐛: fix chores

--- queries/01_get_students_without_github.sql
+++ queries/01_get_students_without_github.sql
@@ -1,3 +1,1 @@
--- select s.id, s."name" as "name" , s.email, s.class_id  from public.students s order by s.class_id;
-
 select s.id, s."name" as "name" , s.email, s.class_id  from public.students s where s.github is null order by s.class_id;
