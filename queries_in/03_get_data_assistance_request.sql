7d0645d9d8ad039bbeb596347672392fe7e19117
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-09 13:34:03 +0900

    FIX🐛: Fixed join problem

--- queries_in/03_get_data_assistance_request.sql
+++ queries_in/03_get_data_assistance_request.sql
@@ -1,10 +1,10 @@
 select
 	i."name" as instructor,
 	s."name" as student,
-	assignments.name as assignment,
-	a.completed_at - a.started_at as assistance_duration
-from students s
-left join assistance_requests a on s.id = a.student_id
-left join instructors i on i.id = a.instructor_id
-left join assignments on assignments.id = a.assignment_id
+	a."name" as assignment,
+	ar.completed_at - ar.started_at as assistance_duration
+from assistance_requests ar
+left join instructors i on i. id = ar.instructor_id
+left join students s on s.id = ar.student_id
+left join assignments a on a.id = ar.assignment_id
 order by assistance_duration asc;
